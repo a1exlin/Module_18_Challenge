c93a1b892e94614b363e0041197f851168e94b2e
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-22 03:07:15 -0500

    added index.js to routes folder

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,4 +1,4 @@
-
+// index.js file added to routes folder and UPDATED
 const router = require('express').Router();
 // Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
 const apiRoutes = require('./api');
