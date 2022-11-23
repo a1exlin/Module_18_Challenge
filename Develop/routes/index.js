203e729e17945c5fcabea089bd0010c7b10cbf04
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-23 02:35:17 -0500

    updated index.js in routes folder

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,10 +1,13 @@
 // index.js file added to routes folder and UPDATED
 const router = require('express').Router();
-// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
-const apiRoutes = require('./api');
+
+const apiRoutes = require('./api/thoughts-routes');
+// const userRoutes = require('./api/user-routes');
 
 // add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
+// router.use('/api', userRoutes);
+
 
 router.use((req, res) => {
   res.status(404).send('<h1> 404 Error!</h1>');
